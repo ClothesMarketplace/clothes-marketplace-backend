6a10c2e75957d661b8670e92a44084ade8f054c9
hotl1ne <105458682+hotl1ne@users.noreply.github.com>
2025-03-06 14:44:07 +0200

    added possibility for hosting on other urls

--- MarketplaceMonolith.Api/Program.cs
+++ MarketplaceMonolith.Api/Program.cs
@@ -70,8 +70,8 @@ builder.Services.AddAutoMapper(typeof(ProfileMapper));
 
 var app = builder.Build();
 
-//var port = Environment.GetEnvironmentVariable("PORT") ?? "8080";
-//app.Urls.Add($"http://*:{port}");
+var port = Environment.GetEnvironmentVariable("PORT") ?? "8080";
+app.Urls.Add($"http://*:{port}");
 
 if (app.Environment.IsDevelopment())
 {
