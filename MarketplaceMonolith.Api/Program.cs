ef29caf259cd9f1b02b37a00db80f81e1546ad66
hotl1ne <105458682+hotl1ne@users.noreply.github.com>
2025-03-10 09:11:25 +0200

    Added `CORS` policy

--- MarketplaceMonolith.Api/Program.cs
+++ MarketplaceMonolith.Api/Program.cs
@@ -34,6 +34,14 @@ builder.Services
         };
     });
 
+builder.Services.AddCors(options =>
+{
+    options.AddPolicy("AllowAll",
+        policy => policy.AllowAnyOrigin()
+                        .AllowAnyMethod()
+                        .AllowAnyHeader());
+});
+
 builder.Services.AddApiVersioning(options =>
 {
     options.DefaultApiVersion = new ApiVersion(1);
@@ -88,6 +96,8 @@ if (app.Environment.IsDevelopment())
 }
 
 app.UseHttpsRedirection();
+
+app.UseCors("AllowAll");
 
 app.UseAuthorization();
 
